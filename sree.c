4a3af2ee8b5799e59cf3f5e4daf7f14f84a6c7a2
srikanth reddy <sanikommusrikanthreddy2@gmail.com>
2026-02-07 12:55:19 +0530

    it is updated version of sri.c

---
+++ sree.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the numbers to add\n");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("the sum of two numbers %d + %d = %d\n",a,b,c);
+    return 0;
+}
